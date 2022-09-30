b365a98708a455aee19eed6dbffb4a0031982fd4
elitonpc\Éliton Melo <elitonp.melo@gmail.com>
2022-09-30 11:04:20 -0300

    custon da page home

--- women-s-store/src/pages/home/home.ts
+++ women-s-store/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ListPage } from '../list/list';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,50 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  items = [
+    'Tênis',
+    'Shorts',
+    'Camisas',
+    'Calças'
+  ];
+
   constructor(public navCtrl: NavController) {
 
+  }
+  categorias = [
+    {
+      id:1,
+      nome:"Tênis",
+      img: "../../assets/imgs/tenis.png"
+    }, {
+      id:2,
+      nome:"Shorts",
+      img: "../../assets/imgs/short.png"
+    }, {
+      id:3,
+      nome:"Camisas",
+      img:"../../assets/imgs/camisa.png"
+    }, {
+      id:4,
+      nome:"Calças",
+      img:"../../assets/imgs/calca.png"
+    }
+  ]
+  itemSelected(categorias: string) {
+    if (categorias == "Camisas"){
+      this.navCtrl.push(ListPage);
+    }
+    if (categorias == "Shorts"){
+      this.navCtrl.push('LoginPage');
+    }
+    if (categorias == "Tênis"){
+      this.navCtrl.push('CadastrarPage');
+    }
+    if (categorias == "Calças"){
+      this.navCtrl.push('RecuperarSenhaPage');
+    }
+   
+    
   }
 
 }
