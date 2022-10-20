1bcbdaa78d4348f15a19a19c6ace8d179801a5e4
elitonpc\Éliton Melo <elitonp.melo@gmail.com>
2022-10-20 11:26:38 -0300

    criacao da page perfil

---
+++ women-s-store/src/pages/perfl/perfl.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PerflPage } from './perfl';
+
+@NgModule({
+  declarations: [
+    PerflPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PerflPage),
+  ],
+})
+export class PerflPageModule {}
