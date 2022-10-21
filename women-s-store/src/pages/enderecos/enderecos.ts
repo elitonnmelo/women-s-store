981fc6c737222ba843e1625e9a837a3a47a9df7b
elitonpc\Éliton Melo <elitonp.melo@gmail.com>
2022-10-21 00:13:08 -0300

    criação das função fechar modal e fechar pagina

--- women-s-store/src/pages/enderecos/enderecos.ts
+++ women-s-store/src/pages/enderecos/enderecos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 /**
  * Generated class for the EnderecosPage page.
@@ -15,11 +15,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EnderecosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public viewCtrl: ViewController,
+    ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EnderecosPage');
+  }
+  fechar() {
+    this.viewCtrl.dismiss();
   }
 
 }
