86bddba0b09bcb5c56e30d0a7e18ea9c19aad2df
elitonpc\Éliton Melo <elitonp.melo@gmail.com>
2022-10-31 18:49:41 -0300

    alterações dados da conta

--- women-s-store/src/pages/dados-da-conta/dados-da-conta.ts
+++ women-s-store/src/pages/dados-da-conta/dados-da-conta.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, ModalController, NavController, NavParams, ViewController } from 'ionic-angular';
 
 /**
  * Generated class for the DadosDaContaPage page.
@@ -14,12 +14,53 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
   templateUrl: 'dados-da-conta.html',
 })
 export class DadosDaContaPage {
+  items = [
+    'Ultimas Compras',
+    'Cupons de Desconto',
+    'Meu carrinho',
+    'Favoritos',
+    'Dados da Conta',
+    'Endereços para entrega',
+    'Formas de Pagamento'
+
+    
+  ];
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public viewCtrl: ViewController,
+    public modalCtrl: ModalController,
     
     ) {
+  }
+  itemSelected(item: string) {
+    console.log("Item selecionado", item);
+    if (item == "Ultimas Compras"){
+      const modal = this.modalCtrl.create('UltimasComprasPage');
+      modal.present();
+    }
+    if (item == "Cupons de Desconto"){
+      const modal = this.modalCtrl.create('CuponsPage');
+      modal.present();
+    }
+    if (item == "Meu carrinho"){
+      this.navCtrl.push('CarrinhoPage');
+    }
+    if (item == "Favoritos"){
+      this.navCtrl.push('FavoritosPage');
+    }
+    if (item == "Dados da Conta"){
+      const modal = this.modalCtrl.create('DadosDaContaPage');
+      modal.present();
+    }
+    if (item == "Formas de Pagamento"){
+      const modal = this.modalCtrl.create('FormasDePagamentoPage');
+      modal.present();
+    }
+    if (item == "Endereços para entrega"){
+      const modal = this.modalCtrl.create('EnderecosPage');
+      modal.present();
+    }
   }
 
   ionViewDidLoad() {
@@ -28,5 +69,6 @@ export class DadosDaContaPage {
   fechar() {
     this.viewCtrl.dismiss();
   }
+
 
 }
